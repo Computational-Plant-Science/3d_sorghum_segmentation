9b2ef07164c97e11d972cd1d9e67155d34939ec2
lsx1980 <suxingliu@gmail.com>
2024-11-07 11:54:26 -0500

    test planit output

--- segmentation.sh
+++ segmentation.sh
@@ -2,6 +2,6 @@
 python3 /opt/code/ai_seg.py -i $INPUT -o $OUTPUT 
 
 # copy nested output files to working directory
-find . -type f -name "*_masked.jpg" -exec cp {} $WORKDIR \;
+#find . -type f -name "*_masked.jpg" -exec cp {} $WORKDIR \;
 
 
